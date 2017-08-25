3d5f03dff7d17859493c0e62635eaade18627d7e
Richard Bradshaw <richardbradshaw@gmail.com>
2017-08-25 12:07:50 +0100

    finished

--- src/test/java/junitlessons/D_Matchers.java
+++ src/test/java/junitlessons/D_Matchers.java
@@ -3,13 +3,13 @@ package junitlessons;
 import org.hamcrest.core.CombinableMatcher;
 import org.junit.Assert;
 import org.junit.Test;
+import org.junit.experimental.categories.Category;
 
 import java.util.Arrays;
 
 import static org.hamcrest.CoreMatchers.*;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertThat;
-
 
 public class D_Matchers
 {
